80866a3aeeba9d35d621cdabeac9778d4a58b3d5
Alexander James Malcolm <alexjamesmalcolm@gmail.com>
2020-10-23 03:56:29 -0400

    Added console.logs to server code

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 import { serve } from "./deps.ts";
 
 const server = serve({ port: 3000 });
+console.log("Starting server on http://0.0.0.0:3000");
 
 for await (const request of server) {
+  console.log("Received request");
   let bodyContent = "Your user-agent is:\n\n";
   bodyContent += request.headers.get("user-agent") || "Unknown";
 
